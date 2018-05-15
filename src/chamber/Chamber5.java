732dca210656756ae77cf9dbb1cbd6204b50c934
Brian Guadalupe <free2sendmail@gmail.com>
2018-05-16 01:28:30 +0800

    minigame for Chamber5 now runs within command line

--- src/chamber/Chamber5.java
+++ src/chamber/Chamber5.java
@@ -2,6 +2,7 @@ package chamber;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 import java.util.Random;
 
 import javax.swing.*;
@@ -18,14 +19,15 @@ public class Chamber5 extends BaseChamber implements ChamberBehavior {
 	
 	private boolean hasAccessed = false;
 	private boolean hasPlayed = false;
+	private boolean gameStarted = false;
+	private boolean genieSummoned = false;
 	
 	// mini-game variables
+	private Random rand = new Random();
 	private int n1 = 0;
 	private int n2 = 0;
 	private int ans = 0;
-	private int timeLeft = 5;
-	
-	private Timer t;
+	private int count = 0;
 
 	@Direction(direction="south", accessible=true, accessMessage="")
 	private Chamber4 south;
@@ -52,7 +54,26 @@ public class Chamber5 extends BaseChamber implements ChamberBehavior {
         return sw.toString();
 	}
 
-
+	@Override
+	public List<String> GetCommands() {
+		List<String> commands = super.GetCommands();
+		commands.remove("follow");
+		commands.remove("ask");
+		commands.remove("answer");
+		if (genieSummoned) commands.add("follow");
+		if (gameStarted) {
+			commands.add("ask");
+			commands.add("answer");
+		}
+		if (hasPlayed) {
+			commands.remove("follow");
+			commands.remove("ask");
+			commands.remove("answer");
+		}
+		
+		return commands;
+	}
+	
 	@Override
 	@Command(command="explore")
 	public String Explore() {
@@ -84,9 +105,8 @@ public class Chamber5 extends BaseChamber implements ChamberBehavior {
 				pw.println("You place the jade pieces into the holes.");
 				pw.println("Everything suddenly shakes and the jade statue glows bright.");
 				pw.println("You hear a loud voice.");
-				TrappedFrame.updateOutput(sw.toString());
-				PlayGame();
-				return "";
+				pw.println("You want to FOLLOW where the voice comes from...");
+				genieSummoned = true;
 			}
 			else
 			{
@@ -100,67 +120,87 @@ public class Chamber5 extends BaseChamber implements ChamberBehavior {
 		return "It doesn't fit.";
 	}
 	
-	public void PlayGame() throws Exception
-	{
-
-		Random rand = new Random();
-		int questionsLeft = 5;
-		
-		while ( !hasPlayed )
-		{
-			JOptionPane.showMessageDialog(null, "A genie appears... \nHe now wants you to prove your prowess in solving math problems.", "Poof!", JOptionPane.INFORMATION_MESSAGE);
-			JOptionPane.showMessageDialog(null, "You can DIE by playing this game.", "A word of warning", JOptionPane.WARNING_MESSAGE);
-			
-			while (questionsLeft-- > 0) {
-				timeLeft = 5;
-				String currStats = String.format("(You currently have %d health, %d hunger)\n", Status.GetHealth(), Status.GetHunger());
-				
-				t = new Timer(1000, new ActionListener() {
-					@Override
-			        public void actionPerformed(ActionEvent e) {
-						System.out.println(timeLeft + " seconds left");
-						if (timeLeft-- <= 0) {
-							Status.RemoveHealth(20); Status.RemoveHunger(20);
-							JOptionPane.showMessageDialog(null, "Too slow! You lose 20 health, 20 hunger. \nPress Enter key in the input box to proceed.", "Time's up!", JOptionPane.ERROR_MESSAGE);
-							t.stop();
-						}
-					}
-				});
-				t.start();
-
-				n1 = rand.nextInt(89) + 10;
-				n2 = rand.nextInt(9) + 1;
-				ans = n1*n2;
-				
-				String playerAnswer = JOptionPane.showInputDialog(String.format("%sWhat is %d * %d?", currStats, n1, n2));
-				t.stop();
-				if (timeLeft > 0) {
-					try {
-						int ansInt = Integer.parseInt(playerAnswer);
-						if (ans == ansInt) {
-							Status.AddHealth(15); Status.AddHunger(15);
-							JOptionPane.showMessageDialog(null, "Correct! You gain 15 health, 15 hunger.", "Whew!", JOptionPane.INFORMATION_MESSAGE);
-						} else {
-							Status.RemoveHealth(20); Status.RemoveHunger(20);
-							JOptionPane.showMessageDialog(null, "Wrong! You lose 20 health, 20 hunger.", "Ouch!", JOptionPane.ERROR_MESSAGE);
-						}
-					} catch (Exception e) {
-						Status.RemoveHealth(20); Status.RemoveHunger(20);
-						JOptionPane.showMessageDialog(null, "Wrong! You lose 20 health, 20 hunger.", "Ouch!", JOptionPane.ERROR_MESSAGE);
-					}
-				}
-			}
-				
-			
-			if (!GameState.PLAYER_DEAD) {
-				JOptionPane.showMessageDialog(null, "The genie lets you live another day...", "Congrats, I guess", JOptionPane.INFORMATION_MESSAGE);
-			} else {
-				JOptionPane.showMessageDialog(null, "And just like that, the genie sucks out your soul.\n It's game over for you...", "Tough luck!", JOptionPane.WARNING_MESSAGE);
-			}
-			
-			hasPlayed = true; //exit condition
-			
-		}
+	@Command(command="follow")
+	public String follow() throws Exception {
+		StringWriter sw = new StringWriter();
+    	PrintWriter pw = new PrintWriter(sw);
+    	
+    	if (!genieSummoned) {
+    		return "Follow what?";
+    	} else {
+    		pw.println("Poof! A genie appears...");
+    		pw.println("He now wants you to prove your prowess in solving math problems.");
+    		pw.println("A word of warning though: You can *die* by playing this game. Good luck!");
+    		pw.println("You may ASK for the first problem.");
+    		gameStarted = true;
+    		generateQuestion();
+    	}
+    	return sw.toString();
+	}
+	
+	@Command(command="ask")
+	public String ask() throws Exception {
+		StringWriter sw = new StringWriter();
+    	PrintWriter pw = new PrintWriter(sw);
+    	
+    	if (!gameStarted) {
+    		return "Huh?";
+    	} else {
+    		if (count > 5) {
+    			if (Status.GetHealth() > 0) {
+    				pw.println("Congrats, I guess.");
+    				pw.println("The genie lets you live another day...");
+    			} else {
+    				pw.println("Tough luck!");
+    				pw.println("And just like that, the genie sucks out your soul. It's game over for you...");
+    				GameState.PLAYER_DEAD = true;
+    			}
+    			hasPlayed = true;
+    		} else {
+    			pw.println("Here goes item " + count + ": " + String.format("What is %d * %d?", n1, n2));
+    			pw.println("Supply your ANSWER by saying: 'answer <number>'");
+    		}
+    	}
+    	return sw.toString();
+	}
+	
+	@Command(command="answer") 
+	public String answer(String playerAnswer) throws Exception {
+		StringWriter sw = new StringWriter();
+    	PrintWriter pw = new PrintWriter(sw);
+    	
+    	if (!gameStarted) {
+    		return "What's that?";
+    	} else {
+    		try {
+    			int ansInt = Integer.parseInt(playerAnswer);
+    			if (ans == ansInt) {
+    				Status.AddHealth(15); Status.AddHunger(15);
+    				pw.println("Correct! You gain 15 health, 15 hunger.");
+    			} else {
+    				Status.RemoveHealth(20); Status.RemoveHunger(20);
+    				pw.println("Wrong! You lose 20 health, 20 hunger.");
+    			}
+    		} catch (Exception e) {
+    			Status.RemoveHealth(20); Status.RemoveHunger(20);
+    			pw.println("Wrong! You lose 20 health, 20 hunger.");
+    		}
+    		if (count < 5)
+    			pw.println("You may ASK for the "+(count == 4 ? "last" : "next")+" problem.");
+    		else
+    			pw.println("You may now ASK the genie about your fate.");
+    		GameState.PLAYER_DEAD = false; // delay their deaths first
+    		generateQuestion();
+    	}
+    	
+    	return sw.toString();
+	}
+	
+	public void generateQuestion() {
+		n1 = rand.nextInt(89) + 10;
+		n2 = rand.nextInt(9) + 1;
+		ans = n1*n2;
+		count++;
 	}
 
 }
